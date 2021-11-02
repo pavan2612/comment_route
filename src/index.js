8b4531ea898c16682b0895f1122321df70b9901c
pavan2612 <pavan2612.m@gmail.com>
2021-11-02 19:58:19 +0530

    deploying

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<BrowserRouter>
+ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}>
                     <App />
                 </BrowserRouter>, document.getElementById('root'));
